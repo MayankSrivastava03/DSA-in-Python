4faf53d0c3f581dd1961e80fe572ea8833ae66f1
Mayank Srivastava <162903950+MayankSrivastava03@users.noreply.github.com>
2024-03-10 22:21:25 +0530

    Create twoSum.py

---
+++ Arrays & Hashing/twoSum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {} #number . index
+        for i in range(len(nums)):
+            remaining = target - nums[i]
+            if remaining in hashmap:
+                return [hashmap[remaining], i]
+            hashmap[nums[i]] = i
+        return
