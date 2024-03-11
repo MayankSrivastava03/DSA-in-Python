1c0857cb1bbfdcf02fe6a3b794d49e4153cb16c9
Mayank Srivastava <162903950+MayankSrivastava03@users.noreply.github.com>
2024-03-11 21:05:12 +0530

    Update Custom Sort String.py

--- Daily Challenge/Custom Sort String.py
+++ Daily Challenge/Custom Sort String.py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        letter_count = {}
+        for char in s:
+            if char in letter_count:
+                letter_count[char] += 1
+            else:
+                letter_count[char] = 1
+        res = ""
+        for i in order:
+            if i in s:
+                while(letter_count[i]!=0):
+                    res += i
+                    letter_count[i] -=1
+        for i in letter_count:
+            while letter_count[i] > 0:
+                res += i
+                letter_count[i] -= 1
+        return res
